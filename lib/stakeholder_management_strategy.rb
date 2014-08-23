387e3519df2b79ff3455f10feea0796061425888
Nigel Lowry <nigel-lowry@ultra.eclipse.co.uk>
2014-08-23 18:01:28 +0100

    Get features running again

--- lib/stakeholder_management_strategy.rb
+++ lib/stakeholder_management_strategy.rb
@@ -1,14 +1,15 @@
 require "stakeholder_management_strategy/version"
-
-@@LEVELS = [:no, :some, :high]
+require 'active_support/all'
 
 module StakeholderManagementStrategy
+  @@LEVELS = [:no, :some, :high]
+
   def self.strategy(power:, interest:)
     raise unless power.in? @@LEVELS
     raise unless interest.in? @@LEVELS
 
     return :ignore if power == :no and interest == :no
-    return :keep_informed if power == :no and (interest == :some or interest == :high)
+    return :keep_informed if power == :no and interest != :no
     return :keep_onside if power == :some
     return :watch if power == :high and interest == :no
     return :keep_satisfied if power == :high and interest == :some
