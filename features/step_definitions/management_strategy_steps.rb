deee1c37e1dd5abfa5fa27387d2bb0de0c69c221
Nigel Lowry <nigel-lowry@ultra.eclipse.co.uk>
2014-08-23 16:49:02 +0100

    Change steps to use world

--- features/step_definitions/management_strategy_steps.rb
+++ features/step_definitions/management_strategy_steps.rb
@@ -1,11 +1,11 @@
-Given(/^the stakeholder has "([^"]*)" power\/influence$/) do |arg|
-  pending
+Given(/^the stakeholder has "([^"]*)" power\/influence$/) do |p|
+  power = p
 end
 
-And(/^they have "([^"]*)" interest in the project$/) do |arg|
-  pending
+And(/^they have "([^"]*)" interest in the project$/) do |i|
+  interest = i
 end
 
-Then(/^the management strategy should be "([^"]*)"$/) do |arg|
-  pending
+Then(/^the management strategy should be "([^"]*)"$/) do |s|
+  expect(StakeholderManagementStrategy.strategy(power: power, interest: interest)).to eq(s)
 end
