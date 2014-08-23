34a6226396331149a31a46e9e6198d4966aacb5d
Nigel Lowry <nigel-lowry@ultra.eclipse.co.uk>
2014-08-23 17:46:41 +0100

    Add some specs and move around

--- spec/stakeholder_management_strategy_spec.rb
+++ spec/stakeholder_management_strategy_spec.rb
@@ -3,8 +3,13 @@ require 'stakeholder_management_strategy'
 
 describe StakeholderManagementStrategy do
   describe '.strategy' do
-    specify { expect { StakeholderManagementStrategy.strategy power: nil, interest: :no }.to raise_error }
+    specify { expect { StakeholderManagementStrategy.strategy }.to raise_error }
+    specify { expect { StakeholderManagementStrategy.strategy power: :no }.to raise_error }
+    specify { expect { StakeholderManagementStrategy.strategy interest: :no }.to raise_error }
+
     specify { expect { StakeholderManagementStrategy.strategy power: :no, interest: nil }.to raise_error }
+    specify { expect { StakeholderManagementStrategy.strategy power: nil, interest: :no }.to raise_error }
     specify { expect { StakeholderManagementStrategy.strategy power: nil, interest: nil }.to raise_error }
+
   end
 end
