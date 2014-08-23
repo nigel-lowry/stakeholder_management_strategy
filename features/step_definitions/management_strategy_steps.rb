94026e1252bee2d84fd38ab3d48cee9261ee318f
Nigel Lowry <nigel-lowry@ultra.eclipse.co.uk>
2014-08-23 16:16:50 +0100

    Generate steps

---
+++ features/step_definitions/management_strategy_steps.rb
@@ -0,0 +1,11 @@
+Given(/^the stakeholder has (.*) power\/influence$/) do |Power|
+  pending
+end
+
+And(/^they have (.*) interest in the project$/) do |Interest|
+  pending
+end
+
+Then(/^the management strategy should be (.*)$/) do |Strategy|
+  pending
+end
